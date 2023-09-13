ea290670e97cf0b1a4565c265ab712d5b8fe1723
TonimatasDEV <amatascubero@gmail.com>
2023-09-13 19:59:14 +0200

    Threads

--- src/main/java/miau/dona/IdeasdeProyecto/Client.java
+++ src/main/java/miau/dona/IdeasdeProyecto/Client.java
@@ -6,35 +6,43 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
-public class Client {
+public class Client implements Runnable {
     public static String testFile = System.getProperty("user.dir") + "\\run\\Prueba.txt";
+    Thread clientThread;
 
-    public static void main() {
-        try (Socket socket = new Socket("localhost", 38528)) {
-            try (OutputStream outputStream = new FileOutputStream(testFile)){
-                Scanner scanner = new Scanner(System.in);
-                String texto;
-                texto = JOptionPane.showInputDialog(null,"Que mensaje quieres enviar?", "Envia un mensaje", JOptionPane.INFORMATION_MESSAGE);
-                byte[] textoBytes = texto.getBytes(StandardCharsets.UTF_8);
-                outputStream.write(textoBytes); // Recoge en forma de bytes lo que se quiere enviar y lo escribe
-
-                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(testFile))){
-                    String linea;
-                    while ((linea = bufferedReader.readLine()) != null) {
-                        JOptionPane.showMessageDialog(null, linea, "Recibiste un mensaje", JOptionPane.INFORMATION_MESSAGE);
-                    }
-                } catch (IOException e) {
-                    JOptionPane.showMessageDialog(null, "Error" + e);
-                }
-            } catch (IOException e) {
-                JOptionPane.showMessageDialog(null, "Error" + e, "Error outputStream", JOptionPane.ERROR_MESSAGE);
-            }
-
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "No se pudo abrir el puerto", "Error puerto", JOptionPane.ERROR_MESSAGE);
-        }
-
-
+    public void startClientThread() {
+        clientThread = new Thread(this);
+        clientThread.start();
     }
 
+    @Override
+    public void run() {
+        while (clientThread != null) {
+
+        }
+
+        //try (Socket socket = new Socket("localhost", 38528)) {
+        //    try (OutputStream outputStream = new FileOutputStream(testFile)){
+        //        Scanner scanner = new Scanner(System.in);
+        //        String texto;
+        //        texto = JOptionPane.showInputDialog(null,"Que mensaje quieres enviar?", "Envia un mensaje", JOptionPane.INFORMATION_MESSAGE);
+        //        byte[] textoBytes = texto.getBytes(StandardCharsets.UTF_8);
+        //        outputStream.write(textoBytes); // Recoge en forma de bytes lo que se quiere enviar y lo escribe
+
+        //        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(testFile))){
+        //            String linea;
+        //            while ((linea = bufferedReader.readLine()) != null) {
+        //                JOptionPane.showMessageDialog(null, linea, "Recibiste un mensaje", JOptionPane.INFORMATION_MESSAGE);
+        //            }
+        //        } catch (IOException e) {
+        //            JOptionPane.showMessageDialog(null, "Error" + e);
+        //        }
+        //    } catch (IOException e) {
+        //        JOptionPane.showMessageDialog(null, "Error" + e, "Error outputStream", JOptionPane.ERROR_MESSAGE);
+        //    }
+
+        //} catch (IOException e) {
+        //    JOptionPane.showMessageDialog(null, "No se pudo abrir el puerto", "Error puerto", JOptionPane.ERROR_MESSAGE);
+        //}
+    }
 }
--- src/main/java/miau/dona/IdeasdeProyecto/Main.java
+++ src/main/java/miau/dona/IdeasdeProyecto/Main.java
@@ -5,8 +5,11 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) { // Sistema de mensajeria
         new File(System.getProperty("user.dir") + "\\run").mkdir();
-        Server.main();
-        Client.main();
 
+        Server server = new Server();
+        server.startServerThread();
+
+        Client client = new Client();
+        client.startClientThread();
     }
 }
