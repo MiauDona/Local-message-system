8b8b09efc04dec6732f4b06642102337a1f6a372
TonimatasDEV <amatascubero@gmail.com>
2023-09-13 15:52:44 +0200

    Change the user directory to run directory

--- src/main/java/miau/dona/IdeasdeProyecto/Client.java
+++ src/main/java/miau/dona/IdeasdeProyecto/Client.java
@@ -7,16 +7,18 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Client {
+    public static String testFile = System.getProperty("user.dir") + "\\run\\Prueba.txt";
+
     public static void main() {
         try (Socket socket = new Socket("localhost", 38528)) {
-            try (OutputStream outputStream = new FileOutputStream("C:\\Users\\donan\\Desktop\\Prueba.txt")){
+            try (OutputStream outputStream = new FileOutputStream(testFile)){
                 Scanner scanner = new Scanner(System.in);
                 String texto;
                 texto = JOptionPane.showInputDialog(null,"Que mensaje quieres enviar?", "Envia un mensaje", JOptionPane.INFORMATION_MESSAGE);
                 byte[] textoBytes = texto.getBytes(StandardCharsets.UTF_8);
                 outputStream.write(textoBytes); // Recoge en forma de bytes lo que se quiere enviar y lo escribe
 
-                try (BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\donan\\Desktop\\Prueba.txt"))){
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(testFile))){
                     String linea;
                     while ((linea = bufferedReader.readLine()) != null) {
                         JOptionPane.showMessageDialog(null, linea, "Recibiste un mensaje", JOptionPane.INFORMATION_MESSAGE);
